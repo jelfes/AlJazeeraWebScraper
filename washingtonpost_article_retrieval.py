6d8ba9b685b8b65b162a78a9872b7d8c333567d2
jelfes <janelfes@gmail.com>
2024-04-18 14:14:16 +0100

    add washington post article retrieval

---
+++ washingtonpost_article_retrieval.py
@@ -0,0 +1,121 @@
+import re
+import pandas as pd
+import logging
+import argparse
+import requests
+
+from csv import writer
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+from pathlib import Path
+from datetime import datetime
+from config_local import DATA_DIR
+from content_parsing import get_paragraphs, merge_paragraphs
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-s",
+    "--start_row",
+    type=int,
+    default=0,
+    help="The first row to start from.",
+)
+# parser.add_argument(
+#     "-p",
+#     "--output_path",
+#     type=str,
+#     default="output.csv",
+#     help="Path to the output file.",
+# )
+
+args = vars(parser.parse_args())
+
+# load urls
+# aljazeera_urls = pd.read_csv(Path(DATA_DIR, "mc_aljazeera_01082022_10032024.csv"))
+washingtonpost_urls = pd.read_csv(
+    Path(DATA_DIR, "mc_washingtonpost_01012023_16042024.csv")
+)
+
+# setup output format
+df_header = pd.DataFrame(
+    {
+        "id": [],
+        "url": [],
+        "title": [],
+        "retrieval_time": [],
+        "web_page": [],
+        "paragraphs": [],
+        "full_text": [],
+        "word_count": [],
+    }
+)
+
+
+# logging
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+# OUTPUT_PATH = Path(args["output_path"])
+OUTPUT_PATH = Path(
+    DATA_DIR, f"ir_data_washingtonpost_{timestamp}_{args['start_row']}.csv"
+)
+
+logging.basicConfig(
+    filename=f"logs/ir_logs_{timestamp}.log",
+    encoding="utf-8",
+    format="%(asctime)s %(message)s",
+    level=logging.INFO,
+)
+logging.captureWarnings(True)
+logging.info("Start logging")
+logging.info(f"Starting row: {args['start_row']}")
+logging.info(f"Output is saved to: {OUTPUT_PATH.absolute()}")
+logging.info("##############################################################\n")
+
+
+# create file if it does not yet exist
+with open(OUTPUT_PATH, "a") as f:
+    df_header.to_csv(f, header=f.tell() == 0, index=False)
+
+# iterate over articles
+url_subset = washingtonpost_urls.iloc[args["start_row"] :].head(10)
+
+
+for row in tqdm(url_subset.itertuples(), total=len(url_subset)):
+    page = requests.get(row.url)
+
+    logging.info(f"Article ID: \t{row.id}")
+    logging.info(f"Article title: \t{row.title}")
+    logging.info(f"Row: \t\t\t{row.Index}")
+    logging.info(f"URL: \t\t\t{row.url}")
+    logging.info(f"Request status:\t{page.status_code}")
+
+    article = BeautifulSoup(page.content, "html.parser")
+    paragraphs = get_paragraphs(article)
+    full_text = merge_paragraphs(paragraphs, "data-el", "text")
+    word_count = len(full_text.split())
+    retrieval_time = datetime.now()
+
+    article_file_path = Path("web_pages", f"{row.id}.html")
+
+    # save article
+    with open(Path(DATA_DIR, article_file_path), "w", encoding="utf-8") as file:
+        file.write(article.prettify())
+
+    new_row = [
+        row.id,
+        row.url,
+        row.title,
+        retrieval_time,
+        article_file_path,
+        paragraphs,
+        full_text,
+        word_count,
+    ]
+
+    with open(OUTPUT_PATH, "a") as f:
+        writer_object = writer(f)
+
+        writer_object.writerow(new_row)
+
+    logging.info("##############################################################\n")
