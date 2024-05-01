85057c27aea13e3f84a0d201565796f687d979e5
jelfes <janelfes@gmail.com>
2024-05-01 12:24:10 +0100

    change id to hash_id

--- aljazeera_article_retrieval.py
+++ aljazeera_article_retrieval.py
@@ -10,7 +10,7 @@ from tqdm import tqdm
 from pathlib import Path
 from datetime import datetime
 from config_local import DATA_DIR
-from content_parsing import get_text_v4
+from content_parsing import get_paragraphs, merge_paragraphs
 
 
 parser = argparse.ArgumentParser()
@@ -21,13 +21,14 @@ parser.add_argument(
     default=0,
     help="The first row to start from.",
 )
-# parser.add_argument(
-#     "-p",
-#     "--output_path",
-#     type=str,
-#     default="output.csv",
-#     help="Path to the output file.",
-# )
+parser.add_argument(
+    "-e",
+    "--end_row",
+    type=int,
+    default=0,
+    help="The row to end.",
+)
+
 
 args = vars(parser.parse_args())
 
@@ -37,10 +38,11 @@ aljazeera_urls = pd.read_csv(Path(DATA_DIR, "mc_aljazeera_01082022_10032024.csv"
 # setup output format
 df_header = pd.DataFrame(
     {
-        "id": [],
+        "hash_id": [],
         "url": [],
         "title": [],
         "retrieval_time": [],
+        "web_page": [],
         "full_text": [],
         "word_count": [],
     }
@@ -50,8 +52,10 @@ df_header = pd.DataFrame(
 # logging
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-# OUTPUT_PATH = Path(args["output_path"])
-OUTPUT_PATH = Path(DATA_DIR, f"ir_data_aljazeera_{timestamp}_{args['start_row']}.csv")
+OUTPUT_PATH = Path(
+    DATA_DIR,
+    f"ir_data_aljazeera_{timestamp}_{args['start_row']}_{args['end_row']}.csv",
+)
 
 logging.basicConfig(
     filename=f"logs/ir_logs_{timestamp}.log",
@@ -61,8 +65,9 @@ logging.basicConfig(
 )
 logging.captureWarnings(True)
 logging.info("Start logging")
-logging.info(f"Starting row: {args['start_row']}")
-logging.info(f"Output is saved to: {OUTPUT_PATH.absolute()}")
+logging.info(f"Starting row:\t\t{args['start_row']}")
+logging.info(f"End row:\t\t\t{args['end_row']}")
+logging.info(f"Output is saved to:\t{OUTPUT_PATH.absolute()}")
 logging.info("##############################################################\n")
 
 
@@ -71,24 +76,39 @@ with open(OUTPUT_PATH, "a") as f:
     df_header.to_csv(f, header=f.tell() == 0, index=False)
 
 # iterate over articles
-url_subset = aljazeera_urls.iloc[args["start_row"] :]  # .head(1000)
+url_subset = aljazeera_urls.iloc[args["start_row"] : args["end_row"]]
 
 
 for row in tqdm(url_subset.itertuples(), total=len(url_subset)):
     page = requests.get(row.url)
 
-    logging.info(f"Article ID: \t{row.id}")
+    logging.info(f"Article ID: \t{row.hash_id}")
     logging.info(f"Article title: \t{row.title}")
     logging.info(f"Row: \t\t\t{row.Index}")
     logging.info(f"URL: \t\t\t{row.url}")
     logging.info(f"Request status:\t{page.status_code}")
 
     article = BeautifulSoup(page.content, "html.parser")
-    full_text = get_text_v4(article)
+    paragraphs = get_paragraphs(article)
+    full_text = merge_paragraphs(paragraphs, "data-el", "text")
     word_count = len(full_text.split())
     retrieval_time = datetime.now()
 
-    new_row = [row.id, row.url, row.title, retrieval_time, full_text, word_count]
+    article_file_path = Path("web_pages", f"{row.hash_id}.html")
+
+    # save article
+    with open(Path(DATA_DIR, article_file_path), "w", encoding="utf-8") as file:
+        file.write(article.prettify())
+
+    new_row = [
+        row.hash_id,
+        row.url,
+        row.title,
+        retrieval_time,
+        article_file_path,
+        full_text,
+        word_count,
+    ]
 
     with open(OUTPUT_PATH, "a") as f:
         writer_object = writer(f)
