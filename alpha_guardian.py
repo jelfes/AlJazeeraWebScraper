26f95e8aab061189285dcff11e16b290b85cecd3
jelfes <janelfes@gmail.com>
2024-07-30 09:47:23 +0200

    drop test files

--- alpha_guardian.py
+++
@@ -1,9 +0,0 @@
-import os
-import requests
-
-API_KEY = os.environ.get("KEY_GUARDIAN")
-
-url = f"https://content.guardianapis.com/search?api-key={API_KEY}"
-page = requests.get(url)
-
-print(page.json())
--- alpha_nyt.py
+++
@@ -1,9 +0,0 @@
-import os
-import requests
-
-API_KEY = os.environ.get("KEY_NYT")
-
-url = f"https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key={API_KEY}"
-page = requests.get(url)
-
-print(page.json())
